25b1a60565f0c55fa05a156b78a8c7ca0d530ebb
joos-mwaiteleke <joos.mwaiteleke@mths.ca>
2024-09-20 11:03:28 -0400

    Made it say the temperature

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jp
+ * Created on: Sep 2024
+ * This program says ther tempoature
 */
 
-basic.showString('Hello, World!')
+let temperature = 0
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    temperature=input.temperature()
+    basic.showString('The temperature is:')
+    basic.showNumber(temperature)
+    })
